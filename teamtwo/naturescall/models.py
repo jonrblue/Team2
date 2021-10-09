5831866364dbc1f69165187114d8218f712f87ca
Jon Blue <jonblue@Jons-MacBook-Air.local>
2021-10-09 10:28:01 -0400

    Part 2 of tutorial: added Restroom model, created superuser

--- teamtwo/naturescall/models.py
+++ teamtwo/naturescall/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Restroom(models.Model):
+    # Set yelp_id max length to 100 based on this:
+    # https://github.com/Yelp/yelp-fusion/issues/183
+    yelp_id = models.CharField(max_length=100)
+    rating = models.FloatField(default=0.0)
--- teamtwo/teamtwo/settings.py
+++ teamtwo/teamtwo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'naturescall.apps.NaturescallConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
