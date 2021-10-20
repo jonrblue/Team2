4b7c45c45400aed43ad6cf1db9d4e88fd0c5af14
Simon Chao <simonchao@Simons-MacBook-Air.local>
2021-10-20 18:03:10 -0400

    Documentation and refactor of class and method names.

--- teamtwo/naturescall/urls.py
+++ teamtwo/naturescall/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'naturescall'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('yelpSearch/', views.yelpSearch, name='yelpSearch'),
-    path('restroom/<int:r_id>', views.restroom, name='restroom'),
-    path('addR/<slug:r_id>', views.addR, name ='addR'),
+    path('search_restroom/', views.search_restroom, name='search_restroom'),
+    path('restroom_detail/<int:r_id>', views.restroom_detail, name='restroom_detail'),
+    path('add_restroom/<slug:r_id>', views.add_restroom, name ='add_restroom'),
 ]
