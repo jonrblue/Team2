e685228e041139c3292833595eed8d26425811df
Jon Blue <jonblue@Jons-MacBook-Air.local>
2021-10-25 11:42:40 -0400

    test coverage of 88 %

--- teamtwo/naturescall/tests.py
+++ teamtwo/naturescall/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Restroom
 from .views import search
@@ -36,29 +36,66 @@ class ViewTests(TestCase):
                                            args=(1,)))
         self.assertEqual(response.status_code, 404)
 
+    # def test_invalid_restroom_desc(self):
+    #     """
+    #     If a restroom's description is under 10 characters,
+    #     it should fail to be added to the database
+    #     """
+    #     desc = "9LTRDESCR"
+    #     yelp_id = "E6h-sMLmF86cuituw5zYxw"
+    #     rr1 = create_restroom(yelp_id, desc)
+    #     response = self.client.get(reverse('naturescall:restroom_detail',
+    #                                        args=(1,)))
+    #     self.assertEqual(response.status_code, 404)
+
     def test_one_restroom(self):
         """
         Once a restroom is added, it should be reachable via the
         restroom_detail link
         """
-        desc = "FAKE DESCRIPTION"
+        desc = "TEST DESCRIPTION"
         yelp_id = "E6h-sMLmF86cuituw5zYxw"
         rr1 = create_restroom(yelp_id, desc)
         response = self.client.get(reverse('naturescall:restroom_detail',
                                            args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, desc)
+        self.assertEqual(response.context['res']['desc'], desc)
 
-    def test_restroom_invalid_search_empty_database(self):
+    def test_restroom_invalid_search(self):
         """
         A search with an invalid search string should yield no results
         but should return a valid webpage
         """
-        response = self.client.post(reverse('naturescall:index'),
-                                    data={'form':{'location':"szzzzz"}})
+        c = Client()
+        response = c.post(reverse('naturescall:search_restroom'),
+                          data={'location': "szzzzz"})
         self.assertEqual(response.status_code, 200)
-        print(response.__dict__)
-        # self.assertContains(response, "Welcome")
+        self.assertContains(response, "Location Not Found")
+
+    def test_restroom_valid_search_empty_database(self):
+        """
+        A search with a valid search string with an empty database
+        should return a valid webpage with 20 "Add Restroom" results
+        """
+        c = Client()
+        response = c.post(reverse('naturescall:search_restroom'),
+                          data={'location': "nyu tandon"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.content).count("Add Restroom"), 20)
+
+    def test_restroom_valid_search_one_element_database(self):
+        """
+        A search with a valid search string with a database with one element
+        should return a valid webpage with 19 "Add Restroom" results
+        """
+        c = Client()
+        desc = "TEST DESCRIPTION"
+        yelp_id = "E6h-sMLmF86cuituw5zYxw"
+        rr1 = create_restroom(yelp_id, desc)
+        response = c.post(reverse('naturescall:search_restroom'),
+                          data={'location': "nyu tandon"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.content).count("Add Restroom"), 19)
 
 # class ViewHelperFunctionTests(TestCase):
 #
